a41173ffd75a72968c55266a91b8b66585e1c992
Tavito <Tavito@192.168.1.74>
2020-07-16 23:26:12 +0200

    probando mi segundo repositorio

---
+++ src/pruebaaa/Pruebaaa.java
@@ -0,0 +1,14 @@
+
+package pruebaaa;
+
+public class Pruebaaa {
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("my nombre es gustavo");
+        System.out.println("Este es mi segundo repositorio");
+    }
+    
+}
