4da289a21f0b8ae57b63b43e7bf08f6a34b36556
Tavito <Tavito@192.168.1.79>
2020-08-01 07:41:36 +0200

    creamos la clase cola

---
+++ src/cola/Cola.java
@@ -0,0 +1,12 @@
+
+package cola;
+
+public class Cola {
+   private Object [] colaArr;                //Arreglo que contiene los elementos de la cila
+   private int tamanoMax;                    //Guarda el tamaño maximo del arreglo
+   private int frente;                       //apunta al primer elemen to de la cola
+   private int fin;                          //apunta añ ultimo elemento de la cola
+   private int numElems;                      //lleva el control dek numero de ele men tod en la cola
+    
+    
+}
