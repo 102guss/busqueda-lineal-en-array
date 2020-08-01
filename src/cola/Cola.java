3dac822e96c49b27351bb8cf06a54da9fe808c9b
Tavito <Tavito@192.168.1.79>
2020-08-01 08:10:40 +0200

    implementacion del contructor cola

--- src/cola/Cola.java
+++ src/cola/Cola.java
@@ -7,6 +7,20 @@ public class Cola {
    private int frente;                       //apunta al primer elemen to de la cola
    private int fin;                          //apunta añ ultimo elemento de la cola
    private int numElems;                      //lleva el control dek numero de ele men tod en la cola
+ //....................................................................................................................
+ //constructor - inicializa valores  de la cola
+
+    public Cola(int tamano) {
+        
+    tamanoMax = tamano;
+    colaArr = new Object [tamanoMax];        // inatancia arreglo que guarda elementos de la cola
+    fin = -1;                                //la vsriable fin la inicializa mos a -1;
+    frente = 0;                              // la variable frente la inicializamos a 0;
+    numElems = 0;                            // numElems se inicializa a 0; (la cola esta vacia , sin argumentos)
+        
+    }
+   
+   
     
     
 }
